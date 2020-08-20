f4389f47064c67c6f2746c7fb1fc8e09660d0369
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-20 15:46:49 +0530

    ADDED:Generated random number as a die output

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -3,4 +3,7 @@ echo "*********************WELCOME TO SNAKE AND LADDER SIMULATION**************"
 #CONSTANT
 PLAYER=1
 PLAYER_START_POSITION=0
-echo "$PLAYER player and its position is $PLAYER_START_POSITION"
+
+#VARIABLE
+random=$((RANDOM%6+1))
+echo "After rolling die Player got $random"
