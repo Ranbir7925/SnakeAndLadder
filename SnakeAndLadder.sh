186f7b9ccb432aef7b6ed35a76cc4301cf463318
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-20 15:40:13 +0530

    ADDED:Player's initial position

---
+++ SnakeAndLadder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+echo "*********************WELCOME TO SNAKE AND LADDER SIMULATION**************"
+#CONSTANT
+PLAYER=1
+PLAYER_START_POSITION=0
+echo "$PLAYER player and its position is $PLAYER_START_POSITION"
