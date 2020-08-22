72094efac86e9c15bc1ecf9d402205f649be6dee
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-22 15:30:32 +0530

    ADDED:Showed the dice count and player's position

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 echo "*********************WELCOME TO SNAKE AND LADDER SIMULATION**************"
+declare -A diceAndPosition
+
 
 #CONSTANT
 PLAYER_START_POSITION=0
@@ -9,16 +11,18 @@ LADDER=2
 SNAKE=3
 
 #VARIABLE
+noOfDice=0
 playerCurrentPosition=$PLAYER_START_POSITION
 
 #Checking the options for player's next move
 function playerNextMove()
 {
+	(( noOfDice++ ))
 	randomValues
 	case $playerMove in
 			$NO_PLAY)
 					playerCurrentPosition=$playerCurrentPosition
-					echo -e "Its NO PLAY move no change will happen..!!\nCurrent postion ---> $playerCurrentPosition\n"
+					echo -e "DICE-->$dieValue\nMOVE-->NO PLAY\nIt's NO PLAY move Player position will be same...!!\nCurrent postion --->$playerCurrentPosition\nPositon After Move -->$playerCurrentPosition\n\n"
 					;;
 			$LADDER)
 					ladderMove
@@ -27,6 +31,7 @@ function playerNextMove()
 					snakeMove
 					;;
 	esac
+	#printf "Reached to position : $timeOfReachedPosition"
 }
 
 
@@ -41,13 +46,13 @@ function randomValues()
 #Function to perform the snake's operation
 function snakeMove()
 {
+	echo -e "DICE-->$dieValue\nMOVE-->SNAKE MOVE\nIt's SNAKE MOVE Player position will be decremented by $dieValue\nCurrent postion --->$playerCurrentPosition"
 	playerCurrentPosition=$(( playerCurrentPosition - dieValue ))
 	if (( playerCurrentPosition < $PLAYER_START_POSITION ))
 	then
 		playerCurrentPosition=$PLAYER_START_POSITION
-		echo $playerCurrentPostion
 	fi
-	echo -e "Its SNAKE move Player's position will be decremented by $dieValue\nCurrent postion ---> $playerCurrentPosition\n"
+	echo -e "Positon After Move -->$playerCurrentPosition\n\n"
 
 }
 
@@ -60,8 +65,10 @@ function ladderMove()
 	then
 		playerCurrentPosition=$(( $playerCurrentPosition - $dieValue ))
 	fi
-	echo -e "Its LADDER move Player's position will be incremented by $dieValue\nCurrent postion ---> $playerCurrentPosition\n"
+	echo -e "DICE-->$dieValue\nMOVE-->LADDER MOVE\nIt's LADDER MOVE Player position will be incremented by $dieValue\nCurrent postion ---> $(( $playerCurrentPosition-$dieValue ))\nPositon After Move -->$playerCurrentPosition\n\n"
+
 }
+
 
 
 function play()
@@ -74,3 +81,4 @@ function play()
 
 
 play
+printf "Total no of Dice Played : $noOfDice"
